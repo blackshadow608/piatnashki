4a53b7b896513c4fa7058bc2992071514268a22e
blackshadow608 <evgen.mosty@yandex.by>
2014-09-20 14:52:31 +0300

    заголовочник

---
+++ Field.h
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#pragma once
+class Field
+{
+	int mas[4][4];
+	int pozX;
+	int pozY;
+
+public:
+	Field();
+	/*! Displays the playing field
+	*/
+	void view();
+	
+	/**Moves the element to the left to position the empty cells*/
+	void moveRight();
+	
+	/**Moves the element to the right to position the empty cells*/
+	void moveLeft();
+	/**Moves the element below the position of the empty cells*/
+	void moveUp();
+	/**Moves the top element of the position of the empty cells*/
+	void moveDown();
+	/**Checks collected field or not*/
+	bool location();
+	/**Generates a random position of the cells of the field*/
+	void rnd();
+
+};
+

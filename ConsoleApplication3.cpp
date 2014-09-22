2a89a75bdc6b03eb83064a224740701798c913c4
Dain_Torson <ales.veshtort@gmail.com>
2014-09-22 20:57:48 +0300

    Added ability to receive cell number. Code was commented

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,84 +1,99 @@
-﻿
+﻿/**@mainpage "Piatnashki" game
+
+*/
 #include <iomanip>
 #include <conio.h>
 #include <iostream>
 #include <clocale>
 #include <ctime>
 #include <cstdlib>
-#include "Field.h"
+#include "header.h"
 
 using namespace std;
 
 
-
+/**@brief Game menu
+@detalied Visualises game menu and control player's choice
+@return Player's choice
+*/
 int menu();
 
 
 void main()
 {
-	int choice = menu();
-	  setlocale (LC_CTYPE, "Russian");
-		  if(choice == 2){cout<<"Данный раздел находится в разработке";};
-		  if(choice == 3){return;}
-	  if (choice == 1)
-	  {
-     Field ptn;
-	 ptn.rnd();
-	 ptn.view();
-	
-	 
-	 char act;
-    int ch;
-    while (1)
-    {
-        act=_getch();
-        ch = static_cast<int>(act);
-     
-       if (ch == 27)
-        {
-            exit(0);
-            break;
-        }
-        else if (ch == 77)
-        {
-			ptn.moveLeft();
-			ptn.view();
-        }
-        else if (ch == 75)
-        {
-			ptn.moveRight();
-			ptn.view();
-        }
-        else if (ch == 72)
-        {
-			ptn.moveUp();
-			ptn.view();
-        }
-        else if (ch == 80)
-        {
-			ptn.moveDown();
-			ptn.view();
-        }
-        
-		if(ptn.location()){cout<<endl;menu();}
-    }
-	  }
+	Field ptn;
+	ptn.rnd();
 
-	 _getch();
+start:
+	int choice = menu();
+	setlocale(LC_CTYPE, "Russian");
+	if (choice == 2){ cout << "Move the empty element to retrieve correct cells position (from 1 to 15)"; };
+	if (choice == 3)
+	{
+		cout << "Enter the cell number (from 0 to 15)" << endl;
+		int ch;
+		cin >> ch;
+		cout << "The value of " << ch << " is " << ptn[ch] << endl;
+		system("PAUSE");
+		goto start;
+	}
+	if (choice == 4){ return; }
+	if (choice == 1)
+	{
+		ptn.view();
+		char act;
+		int ch;
+		while (1)
+		{
+			act = _getch();
+			ch = static_cast<int>(act);
+
+			if (ch == 27)
+			{
+				exit(0);
+				break;
+			}
+			else if (ch == 77)
+			{
+				ptn.moveLeft();
+				ptn.view();
+			}
+			else if (ch == 75)
+			{
+				ptn.moveRight();
+				ptn.view();
+			}
+			else if (ch == 72)
+			{
+				ptn.moveUp();
+				ptn.view();
+			}
+			else if (ch == 80)
+			{
+				ptn.moveDown();
+				ptn.view();
+			}
+
+			if (ptn.location()){ cout << endl; menu(); }
+		}
+	}
+
+	_getch();
 }
 
 
 int menu()
 {
-	system ("cls");
-	setlocale (LC_CTYPE, "Russian");
-	cout<<"1.Play "<<endl<<"2.Rules"<<endl<<"3.Exit"<<endl;
+	system("cls");
+	setlocale(LC_CTYPE, "Russian");
+	cout << "1.Play " << endl << "2.Rules" << endl << "3. Get cell value by cell number" << endl << "4.Exit" << endl;
 	int k;
-	cin>>k;
+	cin >> k;
 	switch (k){
-		case 1:return 1;break;
-		case 2:return 2;break;
-		case 3:return 3;break;
-		default : cout<<"Выберите правильно";
+	case 1:return 1; break;
+	case 2:return 2; break;
+	case 3:return 3; break;
+	case 4:return 4; break;
+	default: cout << "Incorect input. Try again";
 	}
 }
